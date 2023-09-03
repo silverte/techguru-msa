2aa663798a0e6d920e11ba072f5d8dfaadebafdb
silverte <km78cho@gmail.com>
2023-09-03 23:01:54 +0900

    Initial commit

---
+++ test/techguru-msa.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechguruMsa from '../lib/techguru-msa-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/techguru-msa-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechguruMsa.TechguruMsaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
