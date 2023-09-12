9dc2cb7203578db05502bae16f733ecb62437da6
silverte <km78cho@gmail.com>
2023-09-12 14:26:15 +0900

    fix script

--- script/app-deploy-init.sh
+++ script/app-deploy-init.sh
@@ -2,6 +2,7 @@
 
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 export BASE_DIR=~/environment/src
+export STACKS=$(aws cloudformation describe-stacks)
 
 # kafka 라이브러리 다운로드
 mkdir ~/environment/src
@@ -16,13 +17,14 @@ git clone https://github.com/silverte/amazon-msk-spring-boot-eda-order.git $BASE
 # msk topic 생성
 export ZOOKEEPER=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.ExportName=="zookeeper-connection-string") | .OutputValue')
 export BOOTSTRAP_SERVER=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.ExportName=="bootstrap-brokers") | .OutputValue')
+echo $ZOOKEEPER
+echo $BOOTSTRAP_SERVER
 
 $BASE_DIR/kafka_2.12-2.6.2/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 3 --partitions 1 --topic customerTopic
 $BASE_DIR/kafka_2.12-2.6.2/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER --replication-factor 3 --partitions 1 --topic orderTopic
 
 # topic 확인
 $BASE_DIR/kafka_2.12-2.6.2/bin/kafka-topics.sh --bootstrap-server $BOOTSTRAP_SERVER --list
-
 # customer app build
 STACKS=$(aws cloudformation describe-stacks)
 export RDS_ENDPOINT_CUSTOMER=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.ExportName=="rds-endpoint-customer") | .OutputValue')
---
+++ script/cloud9-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# aws cli 설치
+sudo yum remove awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# jq 설치
+sudo yum install -y jq
+
+# kubectl 설치
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+
+# eksctl 설치
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# helm 설치
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+
+# maven 설치 및 설정
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+sudo yum install -y java-11-amazon-corretto.x86_64
+
+# java 버전 변경
+# sudo alternatives --config java
+# sudo alternatives --config javac
